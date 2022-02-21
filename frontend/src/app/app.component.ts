2d68e7c8ddcb9f62406aa489f7490915ccbca57e
Luana Costa <luanacostards@gmail.com>
2022-02-21 14:17:41 -0300

    Realizando primeiras configurações

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './components/template/header/header.component.html',
+  styleUrls: ['./components/template/header/header.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
