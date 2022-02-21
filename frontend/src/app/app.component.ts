8604479e8248c78c1cdef042dcd5430e3496dfb9
Luana Costa <luanacostards@gmail.com>
2022-02-21 17:22:52 -0300

    Adicionando header e rodapé

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './components/template/header/header.component.html',
-  styleUrls: ['./components/template/header/header.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'frontend';
