2ba8d068363d436249d5c1f9ea1fb1608d91a573
Luana Costa <luanacostards@gmail.com>
2022-05-19 11:07:57 -0300

    create(product-create): Creating the method and form for product-create

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,26 +1,31 @@
-import { Router } from '@angular/router';
-import { ProductService } from './../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Product } from "./../product.model";
+import { Router } from "@angular/router";
+import { ProductService } from "./../product.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: "",
+    price: 0,
+  };
 
-  constructor(private productService: ProductService,
-    private router: Router) { }
+  constructor(private productService: ProductService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   createProduct(): void {
-    this.productService.showMessage("Produto criado!")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto criado!");
+      this.router.navigate(["/products"]);
+    });
   }
 
-  
   cancel(): void {
-    this.router.navigate(["/products"])  }
-
+    this.router.navigate(["/products"]);
+  }
 }
